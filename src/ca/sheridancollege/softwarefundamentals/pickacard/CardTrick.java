e55086ff82169407819e293f95441731961899ab
Kennith Yu <yukenn@sheridancollege.ca>
2020-06-07 20:27:40 -0400

    Added functionality to CardTrick.

--- src/ca/sheridancollege/softwarefundamentals/pickacard/CardTrick.java
+++ src/ca/sheridancollege/softwarefundamentals/pickacard/CardTrick.java
@@ -4,6 +4,7 @@
  * and open the template in the editor.
  */
 package ca.sheridancollege.softwarefundamentals.pickacard;
+import java.util.Scanner;
 
 /**
  * A class that fills a magic hand of 7 cards with random Card Objects
@@ -16,17 +17,42 @@ public class CardTrick {
     public static void main(String[] args)
     {
         Card[] magicHand = new Card[7];
+        Scanner scan = new Scanner(System.in);
         
         for (int i=0; i<magicHand.length; i++)
         {
             Card c = new Card();
-            //c.setValue(insert call to random number generator here)
-            //c.setSuit(Card.SUITS[insert call to random number between 0-3 here])
+            c.setValue((int)((Math.random() * 13) + 1));
+            c.setSuit(Card.SUITS[(int)(Math.random() * 4)]);//c.setSuit(Card.SUITS[insert call to random number between 0-3 here])
+            
+            magicHand[i] = c;
         }
         
         //insert code to ask the user for Card value and suit, create their card
+        System.out.println("Pick a card, enter value (1-13):");
+        int userValue = scan.nextInt();
+        
+        System.out.println("Enter suit:");
+        String userSuit = scan.next();
+        
+        boolean found = false;
+        
         // and search magicHand here
+        for (int i = 0; i < magicHand.length; i++) {
+            if (magicHand[i].getSuit().equalsIgnoreCase(userSuit) && magicHand[i].getValue() == userValue) {
+                found = true;
+                break;
+            } else {
+                found = false;
+            }
+        }
+        
         //Then report the result here
+        if (found) {
+            System.out.println("Your card is here! The " + userValue + " of " + userSuit);
+        } else {
+            System.out.println("Sorry your card is not here!");
+        }
     }
     
 }
